3c9a552ebbf4bdee85a3ede04424626895768951
AravindShankarINT024 <aravindshankar2002@gmail.com>
2026-04-05 13:35:04 +0530

    GA Measurement ID modifications

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -4,7 +4,6 @@ export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ''
 
 export const isAnalyticsEnabled = (): boolean => {
   return (
-    process.env.NODE_ENV === 'production' &&
     Boolean(GA_MEASUREMENT_ID)
   );
 };
