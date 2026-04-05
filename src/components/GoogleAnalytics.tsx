a5c8fee2ef09adeb99141fccc08b00c7906fba60
AravindShankarINT024 <aravindshankar2002@gmail.com>
2026-04-05 15:53:48 +0530

    removed logs

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -4,9 +4,7 @@ import { GA_MEASUREMENT_ID, isAnalyticsEnabled } from '@/lib/analytics';
 import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function Analytics() {
-  console.log("GA ID:", GA_MEASUREMENT_ID);
-  console.log("Enabled:", isAnalyticsEnabled());
-  
+
   if (!isAnalyticsEnabled()) return null;
   return <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />;
 }
